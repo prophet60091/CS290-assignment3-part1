931c17762aeabea4170d4afc0cd30933f474ce14
Robert Jackson <jackrobe@onid.oregonestate.edu>
2015-04-21 22:20:26 -0600

    started adding my gists javascript. having trouble connecting added a generic html to start with

--- src/gists.js
+++ src/gists.js
@@ -1,37 +1,39 @@
 function connect(){
 
-var url = 'https://api.github.com/gists/public';
-var req;
+    var url = 'https://api.github.com/gists/public';
+    var req = new XMLHttpRequest();
     var dateOff = (24*60*60*1000); // one day
     var myDate = new Date();
     myDate.setTime(myDate.getTime() - dateOff);
-    var
+    var testing;
 
-    //setup for different browsers to handle activeX shh....
-    if; (window.XMLHttpRequest); { // Mozilla, Safari, IE7+ ...
-        req = new XMLHttpRequest();
-    } else if (window.ActiveXObject) { // IE 6 and older
-        req = new ActiveXObject("Microsoft.XMLHTTP");
-    }
     // make sure it got somewhere
     if (!req)throw 'Something fucked up';
 
-    //Check the state
-    if (req.readyState === 4) {
-        // everything is good, the response is received
-        console.log("it works");
-    } else {
-        console.log("Phail");
-    }
-    //check the response
-    if (req.status === 200) {
-        var testing = JSON.parse(req.responseText);
-    } else {
-        console.log('Received a response other than 200');
-    }
+    req.onreadystatechange= function(){
+        //Check the state
 
- req.open('GET', url);
+        //check the response
+        if (req.status === 200) {
+
+        } else {
+            console.log('Received a response other than 200');
+            console.log(req.statusText);
+        }
+        if (req.readyState === 4) {
+            // everything is good, the response is received
+            console.log("it works");
+            testing = JSON.parse(this.responseText);
+        }
+
+    };
+    req.open('GET', url);
+    req.send();
 
 }
 
+window.onload = function(){
+    connect();
+
+};
 //document.getElementById("connectButton").onclick =function(){connect();};
